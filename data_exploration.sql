d136dde20fad964ef78bea2e5adb79d0fe22a2f2
Christine Yin <yinchiayi@gmail.com>
2023-02-14 18:59:58 -0800

    Update data_exploration.sql

--- data_exploration.sql
+++ data_exploration.sql
@@ -63,8 +63,14 @@ GROUP BY
 ORDER BY
   1;
 
+#######################################################################################################################
+# I am Data Analyst working at CDHealthcare. I have been tasked with recommending three Primary Care Providers (PCPs) #
+# with good customer service for interviews by the operations team to improve our customer experience.                 #
+#######################################################################################################################
+[Presentation Here](#)
 
-# For every individual month in 2018, how many members have a PCP?
+## Slide 3 - Overview of Active Member ##
+# How many active memebers (who have a PCP) in each individual month in 2018
 -- The first step is to convert the start and end dates of the member_provider table to the date data type
 -- Join provider table to determine if a member has a PCP
 WITH converted_date_spans AS (
@@ -137,140 +143,85 @@ FROM
   monthBucket
 ORDER BY
   bucketFirstDay 
-  
 
-# How many members have a PCP in ID in October?
--- Reuse #6 temporary table, add in provider states
-  WITH converted_date_spans AS (
-    SELECT
-      mp.id,
-      mp.member_id,
-      mp.provider_id,
-      mp.plan_id,
-      (
-        20 || LEFT(mp.start_date, 2) || '-' || SUBSTR(mp.start_date, 3, 2) || '-' || RIGHT(mp.start_date, 2)
-      ) :: date AS converted_start_date,
-      CASE
-        WHEN mp.end_date = '999999' THEN '9999-01-01' :: date
-        ELSE (
-          20 || LEFT(mp.end_date, 2) || '-' || SUBSTR(mp.end_date, 3, 2) || '-' || RIGHT(mp.end_date, 2)
-        ) :: date
-      END AS converted_end_date,
-      mp.average_copay,
-      mp.pcp_rating,
-      p.is_pcp AS provider_is_pcp,
-      SPLIT_PART(p.city, ', ', 2) AS provider_states
-    FROM
-      cd_healthcare.member_provider mp
-      LEFT JOIN cd_healthcare.providers p ON mp.provider_id = p.id
-  ) 
--- calculate the total number of members with PCP in ID in November
-SELECT
-  (
+
+## Rest of the slides ##
+-- Create temp tables of all tables for the rest of the analysis (Timestamp data type converted; city and states separated)
+
+WITH converted_all AS (
+  SELECT
+    mp.id AS spans_id,
+    mp.member_id,
+    mp.provider_id,
+    mp.plan_id,
     (
-      SELECT
-        COUNT(member_id)
-      FROM
-        converted_date_pcp_spans
-      WHERE
-        converted_start_date <= '2018-10-31'
-        AND provider_is_pcp = 1
-        AND provider_states = 'ID'
-    ) -(
-      SELECT
-        COUNT(member_id)
-      FROM
-        converted_date_pcp_spans
-      WHERE
-        converted_end_date < '2018-10-01'
-        AND provider_is_pcp = 1
-        AND provider_states = 'ID'
-    )
-  ) AS membersCount 
-  
+      20 || LEFT(mp.start_date, 2) || '-' || SUBSTR(mp.start_date, 3, 2) || '-' || RIGHT(mp.start_date, 2)
+    ) :: date AS converted_start_date,
+    CASE
+      WHEN mp.end_date = '999999' THEN '9999-01-01' :: date
+      ELSE (
+        20 || LEFT(mp.end_date, 2) || '-' || SUBSTR(mp.end_date, 3, 2) || '-' || RIGHT(mp.end_date, 2)
+      ) :: date
+    END AS converted_end_date,
+    mp.average_copay,
+    mp.pcp_rating,
+    p.name AS provider_name,
+    SPLIT_PART(p.city, ', ', 1) AS provider_city,
+    SPLIT_PART(p.city, ', ', 2) AS provider_states,
+    m.name AS member_name,
+    SPLIT_PART(m.city, ', ', 1) AS member_city,
+    SPLIT_PART(m.city, ', ', 2) AS member_states,
+    m.birth_date,
+    m.health_risk_score
+  FROM
+    cd_healthcare.member_provider mp
+    LEFT JOIN cd_healthcare.providers p ON mp.provider_id = p.id
+    LEFT JOIN cd_healthcare.members m ON mp.member_id = m.id
 
-  
-# How many members are currently (as of 12/31/18) seeing a provider practicing outside of the member’s city?
--- Reuse #6 temp table add in provider's city and member's city
-  WITH converted_date_spans AS (
-    SELECT
-      mp.id,
-      mp.member_id,
-      mp.provider_id,
-      mp.plan_id,
-      (
-        20 || LEFT(mp.start_date, 2) || '-' || SUBSTR(mp.start_date, 3, 2) || '-' || RIGHT(mp.start_date, 2)
-      ) :: date AS converted_start_date,
-      CASE
-        WHEN mp.end_date = '999999' THEN '9999-01-01' :: date
-        ELSE (
-          20 || LEFT(mp.end_date, 2) || '-' || SUBSTR(mp.end_date, 3, 2) || '-' || RIGHT(mp.end_date, 2)
-        ) :: date
-      END AS converted_end_date,
-      mp.average_copay,
-      mp.pcp_rating,
-      p.is_pcp AS provider_is_pcp,
-      SPLIT_PART(p.city, ', ', 2) AS provider_states,
-      SPLIT_PART(p.city, ', ', 1) AS provider_city,
-      SPLIT_PART(m.city, ', ', 1) AS member_city
-    FROM
-      cd_healthcare.member_provider mp
-      LEFT JOIN cd_healthcare.providers p ON mp.provider_id = p.id
-      LEFT JOIN cd_healthcare.members m ON mp.member_id = m.id
-  )   
--- calculate the total number of members who see the provider practicing outside of the member's city
-SELECT
-  (
-    (
-      SELECT
-        COUNT(member_id)
-      FROM
-        converted_date_pcp_spans
-      WHERE
-        converted_start_date <= '2018-12-31'
-        AND provider_city != member_city
-    ) -(
-      SELECT
-        COUNT(member_id)
-      FROM
-        converted_date_pcp_spans
-      WHERE
-        converted_end_date < '2018-12-01'
-        AND provider_city != member_city
-    )
-  ) AS membersCount 
-  
+) 
 
-# Create a complete view for the member_provider table (member_provider_converted)
--- Timestamp data type; city and states separated
+## Slide 4 - Distribution of Current Active Member by State ##
+-- currently (as of 12/31/18) 
 SELECT
-  mp.id AS spans_id,
-  mp.member_id,
-  mp.provider_id,
-  mp.plan_id,
-  (
-    20 || LEFT(mp.start_date, 2) || '-' || SUBSTR(mp.start_date, 3, 2) || '-' || RIGHT(mp.start_date, 2)
-  ) :: date AS converted_start_date,
-  CASE
-    WHEN mp.end_date = '999999' THEN '9999-01-01' :: date
-    ELSE (
-      20 || LEFT(mp.end_date, 2) || '-' || SUBSTR(mp.end_date, 3, 2) || '-' || RIGHT(mp.end_date, 2)
-    ) :: date
-  END AS converted_end_date,
-  mp.average_copay,
-  mp.pcp_rating,
-  p.name AS provider_name,
-  SPLIT_PART(p.city, ', ', 1) AS provider_city,
-  SPLIT_PART(p.city, ', ', 2) AS provider_states,
-  m.name AS member_name,
-  SPLIT_PART(m.city, ', ', 1) AS member_city,
-  SPLIT_PART(m.city, ', ', 2) AS member_states,
-  m.birth_date,
-  m.health_risk_score
+  provider_states,
+  COUNT(DISTINCT provider_id) AS total_provider,
+  COUNT(DISTINCT member_id) AS total_memeber
 FROM
-  cd_healthcare.member_provider mp
-  LEFT JOIN cd_healthcare.providers p ON mp.provider_id = p.id
-  LEFT JOIN cd_healthcare.members m ON mp.member_id = m.id
+  converted_all
+WHERE
+  converted_end_date > '2018-12-01' :: date
+  AND provider_id IS NOT NULL
+GROUP BY
+  1
+ORDER BY
+  1 
 
 
-
+## Slide 6 - Rating By States ##
+SELECT
+  provider_states,
+  SUM(
+    CASE
+      WHEN pcp_rating = 5 THEN 1
+      ELSE 0
+    END
+  ) AS Total_five_rating,
+  COUNT(pcp_rating) AS total_give_rating,
+  COUNT(DISTINCT member_id) - COUNT(pcp_rating) AS total_not_give_rating,
+  COUNT(DISTINCT member_id) AS Total_member,
+  SUM(
+    CASE
+      WHEN pcp_rating = 5 THEN 1
+      ELSE 0
+    END
+  ) * 100.0 / COUNT(pcp_rating) AS CSAT,
+  AVG(pcp_rating) AS avg_rating
+FROM
+  converted_all
+WHERE
+  converted_end_date > '2018-12-01' :: date
+  AND provider_id IS NOT NULL
+GROUP BY
+  1
+ORDER BY
+  CSAT DESC 
